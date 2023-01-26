14f8435f43f8ce27d5e315f709a91d101bf0258a
ASZhartun <as.zhartun@gmail.com>
2023-01-26 14:46:35 +0200

    feature: add navigation.xml and stack of title and game fragment behavior

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 
@@ -22,6 +23,9 @@ class TitleFragment : Fragment() {
             container,
             false
         )
+        binding.playButton.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_titleFragment2_to_gameFragment)
+        }
         return binding.root
     }
 }
